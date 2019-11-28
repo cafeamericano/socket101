bb8b638acfb68af11a83002cb1c71568df4f6175
cafeamericano <48388789+cafeamericano@users.noreply.github.com>
2019-11-28 17:43:47 -0500

    Rolled back non Socket IO features; moved to another repo to keep Socket101 repo specifically focused on SocketIO

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-//Cluster
-const cluster = require("cluster");
-
-//Redis
-var redis = require("redis");
-var client = redis.createClient();
-
 //Import express
 var app = require('express')();
 
@@ -15,95 +8,25 @@ var io = require('socket.io')(http);
 //Define our port
 var port = process.env.PORT || 4000;
 
-  // CLUSTER ###################################################################################
+// GENERIC ROUTES ###################################################################################
 
-// Code to run if we're in the master process
-if (cluster.isMaster) {
-  // Count the machine's CPUs
-  var cpuCount = require("os").cpus().length;
-  console.log(cpuCount)
-  // Create a worker for each CPU
-  for (var i = 0; i < cpuCount; i += 1) {
-    cluster.fork();
-  }
+app.get('/', function(req, res){
+  io.emit('chat message', 'A new user has arrived.');
+  res.sendFile(__dirname + '/index.html');
+});
 
-  // Listen for dying workers
-  cluster.on("exit", function(worker) {
-    // Replace the dead worker, we're not sentimental
-    console.log("Worker %d died :(", worker.id);
-    cluster.fork();
-  });
+// SOCKET.IO ###################################################################################
 
-  // Code to run if we're in a worker process
-} else {
-
-  // GENERIC ROUTES ###################################################################################
-
-  app.get('/', function(req, res){
-    io.emit('chat message', 'A new user has arrived.');
-    res.sendFile(__dirname + '/index.html');
-  });
-  
-  // SOCKET.IO ###################################################################################
-
-  io.on('connection', function(socket){
-    socket.on('new-user', function(message){
-      io.emit('new-user', {
-        worker: cluster.worker.id,
-        message: message
-      });
+io.on('connection', function(socket){
+  socket.on('new-user', function(message){
+    io.emit('new-user', {
+      message: message
     });
   });
+});
 
-  // REDIS ROUTES###################################################################################
+// START SERVER ###################################################################################
 
-  //Pull first name
-  app.get("/firstname", (req, res) => {
-    //To set in Redis CLI -> ex) SET firstName "Matthew"
-    client.get("firstName", function(err, reply) {
-      res.send(reply);
-    });
-  });
-
-  //Pull value from string
-  app.get("/:search_key", (req, res) => {
-    client.get(req.params.search_key, function(err, reply) {
-      res.send(reply);
-    });
-  });
-
-  //Pull keys and values from hash
-  app.get("/username/:search_key", (req, res) => {
-    client.hgetall("username:" + req.params.search_key, function(err, reply) {
-      res.json(reply);
-    });
-  });
-
-  //Pull keys and values from hash
-  app.get(
-    "/newhash/:username/:firstname/:lastname/:favoritecolor",
-    (req, res) => {
-      console.log("route hit for adding new hash");
-      let query = [
-        `username:${req.params.username}`,
-        `firstname`,
-        `${req.params.firstname}`,
-        `lastname`,
-        `${req.params.lastname}`,
-        `favoritecolor`,
-        `${req.params.favoritecolor}`
-      ];
-      console.log(query);
-      client.hmset(query, function(err, reply) {
-        res.send(reply);
-      });
-    }
-  );
-  
-  //###################################################################################
-
-  http.listen(port, function(){
-    console.log('Server listening on ' + port);
-  });
-
-}
+http.listen(port, function(){
+  console.log('Server listening on ' + port);
+});
