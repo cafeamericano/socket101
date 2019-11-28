ef4c88ad128b8ef0a386431cd5a08568abca289d
cafeamericano <48388789+cafeamericano@users.noreply.github.com>
2019-11-27 22:25:16 -0500

    Acheived primitive integration of Cluster, SocketIO, Redis, and Express

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+//Cluster
 const cluster = require("cluster");
+
+//Redis
+var redis = require("redis");
+var client = redis.createClient();
 
 //Import express
 var app = require('express')();
@@ -10,9 +15,7 @@ var io = require('socket.io')(http);
 //Define our port
 var port = process.env.PORT || 4000;
 
-
-
-//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  // CLUSTER ###################################################################################
 
 // Code to run if we're in the master process
 if (cluster.isMaster) {
@@ -33,19 +36,74 @@ if (cluster.isMaster) {
 
   // Code to run if we're in a worker process
 } else {
+
+  // GENERIC ROUTES ###################################################################################
+
   app.get('/', function(req, res){
     io.emit('chat message', 'A new user has arrived.');
     res.sendFile(__dirname + '/index.html');
   });
   
+  // SOCKET.IO ###################################################################################
+
   io.on('connection', function(socket){
     socket.on('new-user', function(message){
-      io.emit('new-user', message);
+      io.emit('new-user', {
+        worker: cluster.worker.id,
+        message: message
+      });
     });
-
   });
+
+  // REDIS ROUTES###################################################################################
+
+  //Pull first name
+  app.get("/firstname", (req, res) => {
+    //To set in Redis CLI -> ex) SET firstName "Matthew"
+    client.get("firstName", function(err, reply) {
+      res.send(reply);
+    });
+  });
+
+  //Pull value from string
+  app.get("/:search_key", (req, res) => {
+    client.get(req.params.search_key, function(err, reply) {
+      res.send(reply);
+    });
+  });
+
+  //Pull keys and values from hash
+  app.get("/username/:search_key", (req, res) => {
+    client.hgetall("username:" + req.params.search_key, function(err, reply) {
+      res.json(reply);
+    });
+  });
+
+  //Pull keys and values from hash
+  app.get(
+    "/newhash/:username/:firstname/:lastname/:favoritecolor",
+    (req, res) => {
+      console.log("route hit for adding new hash");
+      let query = [
+        `username:${req.params.username}`,
+        `firstname`,
+        `${req.params.firstname}`,
+        `lastname`,
+        `${req.params.lastname}`,
+        `favoritecolor`,
+        `${req.params.favoritecolor}`
+      ];
+      console.log(query);
+      client.hmset(query, function(err, reply) {
+        res.send(reply);
+      });
+    }
+  );
   
+  //###################################################################################
+
   http.listen(port, function(){
     console.log('Server listening on ' + port);
   });
+
 }
