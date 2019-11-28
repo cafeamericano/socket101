0967f63583c5e1661b1626524d2179ce7f007d02
cafeamericano <48388789+cafeamericano@users.noreply.github.com>
2019-11-27 21:44:59 -0500

    SocketIO seems to be working with Cluster to notify of new user connections

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const cluster = require("cluster");
+
 //Import express
 var app = require('express')();
 
@@ -8,16 +10,42 @@ var io = require('socket.io')(http);
 //Define our port
 var port = process.env.PORT || 4000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
 
-io.on('connection', function(socket){
-  socket.on('new-user', function(message){
-    io.emit('new-user', message);
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// Code to run if we're in the master process
+if (cluster.isMaster) {
+  // Count the machine's CPUs
+  var cpuCount = require("os").cpus().length;
+  console.log(cpuCount)
+  // Create a worker for each CPU
+  for (var i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
+
+  // Listen for dying workers
+  cluster.on("exit", function(worker) {
+    // Replace the dead worker, we're not sentimental
+    console.log("Worker %d died :(", worker.id);
+    cluster.fork();
   });
-});
 
-http.listen(port, function(){
-  console.log('Server listening on ' + port);
-});
+  // Code to run if we're in a worker process
+} else {
+  app.get('/', function(req, res){
+    io.emit('chat message', 'A new user has arrived.');
+    res.sendFile(__dirname + '/index.html');
+  });
+  
+  io.on('connection', function(socket){
+    socket.on('new-user', function(message){
+      io.emit('new-user', message);
+    });
+
+  });
+  
+  http.listen(port, function(){
+    console.log('Server listening on ' + port);
+  });
+}
